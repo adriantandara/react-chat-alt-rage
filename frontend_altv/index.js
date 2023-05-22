9708f6d4cb6d3b4b006c43c178f22d3910da77da
AdrianTandara <adrian.tandara05@gmail.com>
2023-05-23 00:35:41 +0300

    frontend_altv

---
+++ frontend_altv/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App_Rage from './rage/Chat';
+import App_Alt from './altv/Chat';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App_Rage />
+  </React.StrictMode>
+);
